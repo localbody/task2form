cd15d807a176e69001985d3d57a3b26e47203f47
localbody macmini <localbody@gmail.com>
2025-12-14 20:58:10 +0300

    html+css complete

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@
 
       <hr class="form__divider" />
 
+      <!-- step 1 -->
       <div class="form__content">
         <h1 class="form__title">Contact details</h1>
 
@@ -58,8 +59,104 @@
           </div>
         </div>
       </div>
+
+      <!-- step 2 -->
+      <div class="form__content">
+        <h1 class="form__title">Our services</h1>
+
+        <h2 class="form__subtitle">
+          Please select which service you are interested in.
+        </h2>
+
+        <div class="form__items">
+          <div class="form__item">
+            <input type="checkbox" name="" id="development" checked />
+            <label
+              for="development"
+              class="form__item-label form__item-label--development"
+              >Development</label
+            >
+          </div>
+          <div class="form__item">
+            <input type="checkbox" name="" id="web_design" />
+            <label
+              for="web_design"
+              class="form__item-label form__item-label--web_design"
+              >Web Design</label
+            >
+          </div>
+          <div class="form__item">
+            <input type="checkbox" name="" id="marketing" />
+            <label
+              for="marketing"
+              class="form__item-label form__item-label--marketing"
+              >Marketing</label
+            >
+          </div>
+          <div class="form__item">
+            <input type="checkbox" name="" id="other" />
+            <label for="other" class="form__item-label form__item-label--other"
+              >Other</label
+            >
+          </div>
+        </div>
+      </div>
+
+      <!-- step 3 -->
+      <div class="form__content">
+        <h1 class="form__title">What’s your project budget?</h1>
+
+        <h2 class="form__subtitle">
+          Please select the project budget range you have in mind.
+        </h2>
+
+        <div class="form__items">
+          <div class="form__item">
+            <input type="radio" name="budget" id="budget1" checked />
+            <label for="budget1" class="form__item-label"
+              >$5.000 - $10.000</label
+            >
+          </div>
+          <div class="form__item">
+            <input type="radio" name="budget" id="budget2" />
+            <label for="budget2" class="form__item-label"
+              >$10.000 - $20.000</label
+            >
+          </div>
+          <div class="form__item">
+            <input type="radio" name="budget" id="budget3" />
+            <label for="budget3" class="form__item-label"
+              >$20.000 - $50.000</label
+            >
+          </div>
+          <div class="form__item">
+            <input type="radio" name="budget" id="budget4" />
+            <label for="budget4" class="form__item-label">$50.000 +</label>
+          </div>
+        </div>
+      </div>
+
+      <!-- step 4 -->
+      <div class="form__content">
+        <img class="image-ok" src="./assets/images/ok.svg" alt="" />
+
+        <h1 class="form__title center">Submit your quote request</h1>
+
+        <h2 class="form__subtitle center">
+          Please review all the information you previously typed in the past
+          steps, and if all is okay, submit your message to receive a project
+          quote in 24 - 48 hours.
+        </h2>
+
+        <button class="button button--primary button--submit center">
+          Submit
+        </button>
+      </div>
     </div>
-    <button class="button">Next step</button>
+    <div class="buttons">
+      <button class="button button--secondary">Previous step</button>
+      <button class="button button--primary">Next step</button>
+    </div>
   </div>
 </template>
 
